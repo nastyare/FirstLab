003777a9143f49df98ad7e5924dc9a9748db8675
Anastasia <anastasiya.reksius@mail.ru>
2023-02-11 12:27:11 +0700

    Добавьте файлы проекта.

---
+++ firstLab/task1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Число = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Степень = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int resultOfCalculating = 1;
+
+            for (int indexOfNumber = 0; indexOfNumber < n; ++indexOfNumber)
+            {
+                resultOfCalculating *= a;
+            }
+
+            Console.WriteLine(a + "^" + n + " = " + resultOfCalculating);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taskTwo/task2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task2
+{
+    class task2
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int convertedVariable = x;
+
+            do
+            {
+                convertedVariable = convertedVariable / 10;
+                if (convertedVariable < 100)
+                {
+                    do
+                    {
+                        convertedVariable = convertedVariable - 10;
+                    } while (convertedVariable > 10);
+                }
+            } while (convertedVariable > 100);
+
+            int n = int.Parse(x.ToString().Remove(1, 1) + convertedVariable);
+
+            Console.WriteLine("Результат: " + n);
+        }
+    }
+}
